e34981bf39403ef82dde77ed8be6a08c1051b781
kanon <ahsan.buet10@gmail.com>
2021-07-04 20:16:37 +0600

    read excel with  pandas and do multiply!

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello WOrld!")
+import pandas as pd
+import os
+
+# print configuration
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+# file_path = os.getcwd() + "\input_data\Master Report_1526790692.xlsx"
+file_path = os.getcwd() + "\input_data\movies.xls"
+
+# sheet_name = "CM REPORT"
+sheet_name = "2010s"
+
+
+# read excel file
+dfs = pd.read_excel(file_path, sheet_name=sheet_name)
+
+# print pandas dataframe
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+# #     print(dfs)
+
+# multiply aspect ration with budget
+dfs['aspect_ration_budget_multiply'] = dfs['Aspect Ratio'] * dfs['Budget']
+
+dfs['aspect_ration_budget_multiply'].fillna(1, inplace=True)
+# print pandas dataframe
+print(dfs.head(n=40))
